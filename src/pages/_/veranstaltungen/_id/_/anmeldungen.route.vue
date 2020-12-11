86154117e8e57943533d004e9779b35739902a48
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-11 20:06:03 +0100

    fix: dont count MA in anzahlen and count MA seprate - sort MA extra

--- src/pages/_/veranstaltungen/_id/_/anmeldungen.route.vue
+++ src/pages/_/veranstaltungen/_id/_/anmeldungen.route.vue
@@ -3,9 +3,9 @@ v-card-text(style='overflow: auto')
   p(style='font-size: 12pt')
     b Nicht abgemeldet und nicht auf Warteliste:
     br
-    | Männlich: {{ data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.person.geschlecht === "m").length }}
+    | Männlich: {{ data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.person.geschlecht === "m" && a.position === 1).length }}
     br
-    | Weiblich: {{ data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.person.geschlecht === "w").length }}
+    | Weiblich: {{ data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.person.geschlecht === "w" && a.position === 1).length }}
     br
     | Gesamt: {{ data.anmeldungen.filter((a) => a.wartelistenPlatz === 0).length }}
   v-list(two-line)
@@ -21,7 +21,17 @@ v-card-text(style='overflow: auto')
         v-list-tile-sub-title Rolle: {{ rollen[anmeldung.position - 1] }}
     v-list-tile(
       @click='$router.push({ path: `/anmeldungen/${anmeldung.anmeldeID}/home`, query: { prev: $route.fullPath } })',
-      v-for='(anmeldung, c) in data.anmeldungen.filter((a) => a.wartelistenPlatz === 0)',
+      v-for='(anmeldung, c) in data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.position === 1).sort((a, b) => b.position - a.position)',
+      :class='`wlist-${anmeldung.wartelistenPlatz} wlist`'
+    )
+      v-list-tile-action
+        | {{ c + 1 }}
+      v-list-tile-content
+        v-list-tile-title {{ anmeldung.person.vorname }} {{ anmeldung.person.nachname }} ({{ anmeldung.person.gebDat.german }}) | {{ getTitle(anmeldung.wartelistenPlatz) }}
+        v-list-tile-sub-title Rolle: {{ rollen[anmeldung.position - 1] }}
+    v-list-tile(
+      @click='$router.push({ path: `/anmeldungen/${anmeldung.anmeldeID}/home`, query: { prev: $route.fullPath } })',
+      v-for='(anmeldung, c) in data.anmeldungen.filter((a) => a.wartelistenPlatz === 0 && a.position > 1)',
       :class='`wlist-${anmeldung.wartelistenPlatz} wlist`'
     )
       v-list-tile-action
