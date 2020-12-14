8c6393d473a498e9d79f3441d664579fb73d76ae
Sebastian Krüger <2pi_r2@gmx.de>
2020-12-14 14:04:57 +0100

    add erzeugt_durch fzantrag

--- src/pages/_/personen/_id/_.route.vue
+++ src/pages/_/personen/_id/_.route.vue
@@ -508,6 +508,7 @@ export default class EcRootIndex extends Vue {
               erzeugt {
                 german
               }
+              erzeugt_durch
             }
             # datumDesLetztenFZ {
             #   german
